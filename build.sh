fecc4bc407344be00f6c9bbfbed5615230c0f343
Kai Marshland <kaimarshland@gmail.com>
2017-01-22 18:14:06 -0800

    Switched to using ocaml build instead of corebuild

--- build.sh
+++ build.sh
@@ -2,7 +2,17 @@
 # opam pin cohttp https://github.com/danielrichman/ocaml-cohttp#v0.20.2-hotpatch
 # opam install core async cohttp ctypes ctypes-foreign 
 
-corebuild \
+
+ocamlbuild \
+    -use-ocamlfind \
+    -pkg core \
+    -tag "ppx(ppx-jane -as-ppx)" \
+    -tag thread \
+    -tag debug \
+    -tag bin_annot \
+    -tag short_paths \
+    -cflags "-w A-4-33-40-41-42-43-34-44" \
+    -cflags -strict-sequence \
     -pkg ctypes.foreign \
     -pkg async \
     -pkg cohttp.async \
